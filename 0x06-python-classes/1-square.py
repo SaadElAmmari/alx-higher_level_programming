cc68d8fdef66b0f1a556c2e2d99e3393b0f96c80
SaadElAmmari <giochi6@hotmail.com>
2024-10-01 11:30:30 -0700

    hey

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Squaree."""
+
+
+class Squaree:
+    """Represent a squaree."""
+
+    def __init__(self, size):
+        """Initialize a new Squaree.
+
+        Args:
+            size (int): The size of the new squaree.
+        """
+        self.__size = size`
